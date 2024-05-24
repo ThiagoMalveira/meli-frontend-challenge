1b6a1d5872c7e4b68c4a786ff2e05d827864af43
thiagomalveira <thiago.a.malveira@gmail.com>
2024-05-24 09:44:40 -0300

    feat(input): request on press enter

--- app/pages/components/Input/index.tsx
+++ app/pages/components/Input/index.tsx
@@ -1,3 +1,4 @@
+import { KeyboardEvent } from "react";
 import Button from "../Button";
 import { SearchIcon } from "../SearchIcon";
 import styles from "./styles.module.css";
@@ -5,6 +6,16 @@ import styles from "./styles.module.css";
 type IInput = {
   handleChange: (newSearchTerm: string) => void;
   action: () => void;
+};
+
+const handleKeyDown = async (
+  event: KeyboardEvent<HTMLInputElement>,
+  action: () => void
+) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    action();
+  }
 };
 
 export default function Input({ handleChange, action }: Readonly<IInput>) {
@@ -15,6 +26,9 @@ export default function Input({ handleChange, action }: Readonly<IInput>) {
         className={styles.searchInput}
         placeholder="Nunca dejes de buscar"
         onChange={(event) => handleChange(event.target.value)}
+        onKeyDown={(event: KeyboardEvent<HTMLInputElement>) =>
+          handleKeyDown(event, action)
+        }
         autoCapitalize="off"
         autoCorrect="off"
         spellCheck="false"
